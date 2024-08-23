b4ae8fe23ef2e2dda5378712223662b1880a90bb
Sarthak vyas <142726436+Sarthak-vyas183@users.noreply.github.com>
2024-08-23 17:30:24 +0530

    DB-network issue fixed

--- utiles/DB.js
+++ utiles/DB.js
@@ -2,13 +2,17 @@ const mongoose = require("mongoose")
 const URI = process.env.Mongodb_URI;
 
 const dbconnet = async () => {
-    console.log("connecting.....")
-    try {
-       await mongoose.connect(URI);
-       console.log("connection success")
-    } catch (error) {
-       console.log("connection failed", error) 
-    }
+   try {
+      console.log("connecting.....")
+      try {
+         await mongoose.connect(URI);
+         console.log("connection success")
+      } catch (error) {
+         console.log("connection failed", error) 
+      }
+   } catch (error) {
+      res.send(`Internal server or Network error ! ${error}`)
+   }
 }
 
 module.exports = dbconnet;
