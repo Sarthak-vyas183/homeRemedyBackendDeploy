cda4b71742cceab19921ee8ecc0f2b514caa43c8
Sarthak vyas <142726436+Sarthak-vyas183@users.noreply.github.com>
2024-08-30 14:43:47 +0530

    testing:

--- server.js
+++ server.js
@@ -1,35 +1,31 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const cors = require("cors")
+const cors = require("cors");
 const dbconnet = require("./utiles/DB");
 const userRouter = require("./Router/userRouter");
-const AuthRouter = require("./Router/AuthRouter")
+const AuthRouter = require("./Router/AuthRouter");
 const DoctorRouter = require("./Router/Doctor.router");
 const AdminRouter = require("./Router/AdminRouter");
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 var corsOptions = {
-  origin: ['http://localhost:5173' , "https://home-made-remedies.netlify.app"],
+  origin: ["http://localhost:5173", "https://home-made-remedies.netlify.app"],
   methods: "GET, POST, PUT, DELETE",
-  credentials: true
+  credentials: true,
 };
-  app.use(cors(corsOptions)) 
-dbconnet()
+app.use(cors(corsOptions));
+dbconnet();
 
+app.get("/", (req, res) => {
+  res.send("welcome to the world of Backend");
+});
+app.use("/api/user", userRouter);
+app.use("/api/auth", AuthRouter);
+app.use("/api/doctor", DoctorRouter);
+app.use("/api/admin", AdminRouter);
 
-
-
-app.get("/" , (req, res)=>{res.send("welcome to the world of Backend")})
-app.use("/api/user" , userRouter)
-app.use("/api/auth" , AuthRouter)
-app.use("/api/doctor" ,DoctorRouter)
-app.use("/api/admin" , AdminRouter)
-
-
-
-app.listen(3000 , () => {
-    console.log("server is listning")
-})
+app.listen(3000, () => {
+  console.log("server is listning");
+});
